eba6075be6b049d5904959e28e0c6f27aca6a3dd
FaV <auras717@yahoo.com>
2021-12-29 02:23:08 +0200

    Release Version 1.0 finished, everything works LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- bot.py
+++ bot.py
@@ -4,10 +4,15 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 reaction = 0
 date_time = datetime.now()
+global embed_search 
 embed_search = discord.Embed
+global page_nr
+page_nr = int
 
 
 async def create_embed(link, a, message):
+    global embed_search
+    global page_nr
     embed_search = discord.Embed(
         title = "Searching for: " + a,
         color = discord.Color.blue(),
@@ -15,36 +20,49 @@ async def create_embed(link, a, message):
     embed_search.set_footer(text ="Please let me know if any bugs appear!")
     #get the name of the light novel and modify it for the link
     embed_search.add_field(
+        name = "Page Number",
+        value = page_nr,
+        inline = False
+    )
+    embed_search.add_field(
         name = "Direct link to search: ",
         value = link,
         inline = False,
     )
     scrape(link, embed_search)
-    await message.channel.send(embed=embed_search)
+    if Nothing_found == True:
+        await message.channel.send("Nothing found")
+    else:
+        await message.channel.send(embed=embed_search)
     
 
 def scrape(link, embed_search):
+    global Nothing_found
+    Nothing_found = False
     URL = link #the URL to request
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(id = "posts") #the id to find the needed data
-    job_elements = results.find_all("h1", class_ = "post-title entry-title" ) #more accurate data retrieval
-    for job_element in job_elements:
-        name = job_element.find("a", rel = "bookmark") #data retrieval
-        title = name.text #get the light novel name so it can be manipulated
-        nume = name.text #get the light novel name so it can be used in the embed
-        title = title.replace("–","") #weird character used in the site :/
-        title = title.replace(" ","-") #to create the link
-        title = title.replace(" ","-") #another weird character used in the site :/ (ALT + 255)
-        title = title.replace("[","") #to create the link
-        title = title.replace("]","") #to create the link
-        link = "https://jnovels.com/" #default light novel link
-        link = link + title #get the final light novel link
-        embed_search.add_field(
-            name = nume,
-            value = link,
-            inline = False,
-        )
+    try:
+        job_elements = results.find_all("h1", class_ = "post-title entry-title" ) #more accurate data retrieval
+        for job_element in job_elements:
+            name = job_element.find("a", rel = "bookmark") #data retrieval
+            title = name.text #get the light novel name so it can be manipulated
+            nume = name.text #get the light novel name so it can be used in the embed
+            title = title.replace("–","") #weird character used in the site :/
+            title = title.replace(" ","-") #to create the link
+            title = title.replace(" ","-") #another weird character used in the site :/ (ALT + 255)
+            title = title.replace("[","") #to create the link
+            title = title.replace("]","") #to create the link
+            link = "https://jnovels.com/" #default light novel link
+            link = link + title #get the final light novel link
+            embed_search.add_field(
+                name = nume,
+                value = link,
+                inline = False,
+            )
+    except AttributeError:
+        Nothing_found = True
 
 
 client = discord.Client()
@@ -59,13 +77,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        await message.add_reaction("◀️") #adding reaction the message so the embed can be changed
-        await message.add_reaction("▶️") #adding reaction the message so the embed can be changed
+        try:
+            if message.embeds[0] != None:
+                await message.add_reaction("◀️") #adding reaction the message so the embed can be changed
+                await message.add_reaction("▶️") #adding reaction the message so the embed can be changed
+        except IndexError:
+            return
     if message.content.startswith('&help'):
         await message.channel.send('&find <light novel name>')
     if message.content.startswith('&find'):
         global reaction
         reaction = 0
+        global page_nr
+        page_nr = 1
         await search(message)
 
 
@@ -76,9 +100,8 @@ async def search(message):
         link_text = link_text[6:] #deleting &find 
         a = link_text
         link_text = link_text.replace(' ', '+') #replacing spaces for the link
-        link = "https://jnovels.com/page/" #the default novel link
-        page = "1/?s="
-        link = link + page + link_text #make the integral link starting at page 1
+        link = "https://jnovels.com/page/1/?s=" #the default novel link
+        link = link + link_text #make the integral link starting at page 1
         await create_embed(link, a, message) #awaitable coroutine so it works dont ask me why it needs to be like this
     except AttributeError:
             await message.channel.send("Nothing found...")
@@ -93,14 +116,79 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
         message_time = int(round(message.created_at.timestamp()))
         request_time = int(datetime.now().timestamp())
         if (message_time + 7500 > request_time):
-            print("on time")
-            print(str(payload.emoji))
             if (str(payload.emoji) == '◀️' ):
-                print("left")
+                await edit_left(message)
             elif (str(payload.emoji) == '▶️' ):
-                print("right")
+                await edit_right(message)
         else:
-            print("too late")
+            return
+
+
+async def edit_left(message):
+    global embed_search
+    global page_nr
+    page_nr = int(message.embeds[0].fields[0].value)
+    if page_nr == 1:
+        await message.channel.send("Can't go below 1st page!")
+    else:
+        page_nr = page_nr - 1
+    text = message.embeds[0].title
+    text_link = message.embeds[0].title[15:].replace(" ","-") #to create the link
+    link = "https://jnovels.com/page/" + str(page_nr) + "/?s=" + text_link
+    new_search = discord.Embed(
+        title = text,
+        color = discord.Color.blue(),
+    )
+    new_search.set_footer(text ="Please let me know if any bugs appear!")
+    #get the name of the light novel and modify it for the link
+    new_search.add_field(
+        name = "Page Number: ",
+        value = page_nr,
+        inline = False,
+    )
+    new_search.add_field(
+        name = "Direct link to search: ",
+        value = link,
+        inline = False,
+    )
+    embed_search = new_search
+    scrape(link, embed_search)
+    if Nothing_found == True:
+        await message.channel.send("No more pages")
+    else:
+        await message.edit(embed = embed_search)
+
+
+async def edit_right(message):
+    global embed_search
+    page_nr = int(message.embeds[0].fields[0].value)
+    page_nr = page_nr + 1
+    text = message.embeds[0].title
+    text_link = message.embeds[0].title[15:].replace(" ","-") #to create the link
+    link = "https://jnovels.com/page/" + str(page_nr) + "/?s=" + text_link
+    new_search = discord.Embed(
+        title = text,
+        color = discord.Color.blue(),
+    )
+    new_search.add_field(
+        name = "Page Number: ",
+        value = page_nr,
+        inline = False,
+    )
+    new_search.set_footer(text ="Please let me know if any bugs appear!")
+    new_search.add_field(
+        name = "Direct link to search: ",
+        value = link,
+        inline = False,
+    )
+    embed_search = new_search
+    scrape(link, embed_search)
+    if Nothing_found == True:
+        await message.channel.send("No more pages")
+        print(message.embeds[0].fields[0].value)
+        message.embeds[0].fields[0].value = str(int(message.embeds[0].fields[0].value) - 1)
+    else:
+        await message.edit(embed = embed_search)
 
 
 #◀️
---
+++ test filepy.py
@@ -0,0 +1,7 @@
+#test file
+try:
+    a_int = "5"
+    print(5 + int(a_int))
+except ValueError:
+    print("error")
+#ValueError
